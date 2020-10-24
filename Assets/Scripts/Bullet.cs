016dfa854bbf3e445544be1cc0244418c400816c
jeongmin-ahn122 <jeongmin.ahn.122@gmail.com>
2020-10-24 11:52:36 -0700

    Shooting Works!

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if(hit.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
