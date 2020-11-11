f205499a918eaf1701a389f9b6998b65d53510c8
jeongmin-ahn122 <jeongmin.ahn.122@gmail.com>
2020-11-11 12:37:24 -0800

    Made Enemy and PowerUp ScritableObjects

---
+++ Assets/Scripts/ScriptableObjects/Enemies/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Enemy")]
+
+public class Enemy : ScriptableObject
+{
+    public enum ENEMY_TYPE
+    {
+        STATIONARY,
+        BASIC,
+        BASHER,
+    }
+
+    public ENEMY_TYPE enemyType;
+    public Sprite sprite;
+
+    [Header("Enemy Attack")]
+    public float bulletSpeed;
+    public float detectRange;
+    public float attackSpeed;
+    public int damage;
+
+    [Header ("Health")]
+    public int maxHealth;
+}
