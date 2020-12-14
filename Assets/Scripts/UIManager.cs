859d3609f5961528378b5259908ec5a6776a203e
Natalie Bonomo <natalie.bonomo727@myci.csuci.edu>
2020-12-13 22:13:23 -0800

    Spinning Icons and Pause Menu
    Spin Spin and Menu Update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,26 +8,31 @@ public class UIManager : MonoBehaviour
 
     public GameObject restartDialog;
     public Animator dialog;
-    
+
 
 
 
     public void StartGame()
-{
-    SceneManager.LoadScene("SampleScene");
-}
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
 
     public void OpenSettings()
     {
-        
-           dialog.SetBool("isHidden", false);
+
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void OpenPause()
+    {
+
+        dialog.SetBool("isHidden", false);
+
     }
     public void CloseSettings()
     {
-
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
         dialog.SetBool("isHidden", true);
     }
-
-
-
 }
